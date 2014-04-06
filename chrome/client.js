c83f7cd8d9af919484e8ea69c8e029a7ec64a838
Iris <iriswang@berkeley.edu>
2014-04-05 17:25:36 -0700

    bidirectional connection works

--- chrome/client.js
+++ chrome/client.js
@@ -1,25 +1,22 @@
 var client = new BinaryClient('ws://localhost:9000');
-var stream = client.createStream();
+var stream;
 var transactionID = 0;
 var pending = {};
 
-stream.on('data', function(data) {
-    var funct = pending[data.transactionID];
-    funct(data); 
+client.on('open', function() {
+    stream = client.createStream();
+    stream.on('data', function(data) {
+        console.log("Received data:", data);
+        var funct = pending[data.ID];
+        funct(data); 
+    });
 });
-
-var transactionId = 0;
 
 function register(file, userAccounts) {
     stream.write({photo: file, action: 'register', accounts: userAccounts});
 }
 
-function authenticate(file) {
-    serverAuth(file, function(data) {
-        return data;
-    });
-}
-function serverAuth(file, callback) {
+function authenticate(file, callback) {
     stream.write({ID: transactionID, photo: file, action: 'authenticate'});
     pending[transactionID] = callback; 
     transactionID ++;
--- chrome/server.js
+++ chrome/server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 var server = http.createServer(app);
 
 // Start Binary.js server
-var BinaryServer = require('../../').BinaryServer;
+var BinaryServer = require('binaryjs').BinaryServer;
 var bs = BinaryServer({server: server});
 
 // Wait for new user connections
@@ -17,10 +17,36 @@ bs.on('connection', function(client){
     // Incoming stream from browsers
     client.on('stream', function(stream){
         stream.on('data', function(data){
-            console.log(data);
+            var userdata = data;
+            if (userdata.action == 'register') {
+                register(data.photo, data.accounts);
+                console.log(data);
+            } else if (userdata.action == 'authenticate') {
+                var result = authenticate(data.photo, data.ID);
+                console.log(data);
+                stream.write(result);
+            } else {
+                console.log("this is bad! it's not matching any of the actions");
+                console.log(data);
+            }
         });
     });
 });
+
+function authenticate(file, ID) {
+    console.log("I'm authenticating!");
+    console.log("file is: ");
+    console.log(file);
+    return {"ID": ID, "username": "iris", "password": "hola"};
+}
+
+function register(file, accounts) {
+    console.log("I'm registering!");
+    console.log("file is: ");
+    console.log(file);
+    console.log("accounts are: ");
+    console.log(accounts);
+}
 
 server.listen(9000);
 console.log('HTTP and BinaryJS server started on port 9000');
