25cb63cbd27589c43ef922a77bdb73f927585779
Michelle Hwang <michellerubyhwang@gmail.com>
2014-04-06 02:57:42 -0700

    adding test files

--- server/biome-app/db/test/user-test.js
+++ server/biome-app/db/test/user-test.js
@@ -3,8 +3,8 @@ var DB = require('../db.js');
 var User = require('./../user.js').User;
 var Account = require('./../account.js').Account;
 
-var michelle = new User({ name : "michelle", accounts: {} });
-var iris = new User({ username : "iriswang", accounts: {}});
+var michelle = new User({ uid:0, imgPaths: ["hihihi", "hel"], name : "michelle", accounts: {} });
+var iris = new User({ uid: 1,imgPaths:["fake"], username : "iriswang", accounts: {}});
 
 michelle.save();
 iris.save();
@@ -12,13 +12,47 @@ iris.save();
 michelle.addAccount("dropbox", "michellehwang", "dropbox");
 michelle.addAccount("facebook", "michellehwang", "facbeook");
 michelle.addAccount("google", "michellehwang", "google");
-iris.addAccount("iris", "fbuname", "pw")
+iris.addAccount("iris", "fbuname", "pw");
 
-michelle.deleteAccount("google")
+michelle.deleteAccount("google");
 
+
+console.log("BEGIN OTHER STUF")
+// var usr = michelle.findIdByPath
 //teardown
-User.find(function(err, results) {
+function findIdByPath(imgPath, callback) {
+	var cb = callback;
+    var usr = User.find({imgPaths:imgPath}, function(err, users) {
+        // for (user in users) {
+        //     paths = users[user].imgPaths;
+        //     for (var i = 0; i < paths.length; i++) {
+        //         if (paths[i] == imgPath) {
+        //             return users[user].uid
+        //             // return users[user]._id;
+        //         }
+        //     }
+        // }
+        if (users.length > 0) {
+        	callback(users[0]); return;
+        }
+        callback(null);
+    });
+    if (usr == null) {
+    	console.log("could not find usr");
+    } else {
+    	return usr;
+    }
+};
+
+findIdByPath(michelle.imgPaths[0], function(results) {
 	console.log(results);
+});
+findIdByPath(michelle.imgPaths[0], function(results) { 
+	console.log(results);
+});
+
+User.find(function(err, results) {
+	// console.log("res"+results);
 	for (res in results) {
 		results[res].remove();
 	}
