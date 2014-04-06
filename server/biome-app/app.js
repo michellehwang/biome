904ba7221c38e5ddaff2db94cab7d064768e4e2e
Iris <iriswang@berkeley.edu>
2014-04-05 19:35:08 -0700

    added binaryjs

--- server/biome-app/app.js
+++ server/biome-app/app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 
@@ -35,6 +36,72 @@ app.get('/users', user.list);
 app.post('/create', routes.create);
 app.delete('/del', routes.del);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var server = http.createServer(app);
+
+// Start Binary.js server
+var BinaryServer = require('binaryjs').BinaryServer;
+var bs = BinaryServer({server: server});
+
+var imgCount = 0;
+
+// Wait for new user connections
+bs.on('connection', function(client){
+    console.log("Connection");
+    // Incoming stream from browsers
+    client.on('stream', function(stream){
+        stream.on('data', function(data){
+            var userdata = data;
+            if (userdata.action == 'register') {
+                serverReg(data.photo, data.accounts);
+                console.log(data);
+            } else if (userdata.action == 'authenticate') {
+                var result = serverAuth(data.photo, data.ID);
+                console.log(data);
+                stream.write(result);
+            } else {
+                console.log("this is bad! it's not matching any of the actions");
+                console.log(data);
+            }
+        });
+    });
 });
+
+function serverAuth(file, ID) {
+    console.log("I'm authenticating!");
+    console.log("file is: ");
+    console.log(file);
+
+    var fs = require('fs');
+    fs.writeFile("images/temp.png", file, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    }); 
+
+    return {'ID': ID, 'userAccounts' : {'facebook' : {'username' : 'iris', 'password' : 'hola'}}}
+}
+
+function serverReg(file, accounts) {
+    var fs = require('fs');
+    var imgPath = "images/img_" + imgCount + ".png";
+    imgCount++; 
+    fs.writeFile(imgPath, file, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    }); 
+    console.log("I'm registering!");
+    console.log("file is: ");
+    console.log(file);
+    console.log("accounts are: ");
+    console.log(accounts);
+}
+
+server.listen(9000);
+console.log('HTTP and BinaryJS server started on port 9000');
+
+
